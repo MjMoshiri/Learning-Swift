5fa23d37253ff1d70b09d247df193a27e32efa32
MJ Moshiri <thisismjmoshiri@gmail.com>
2025-09-29 18:57:10 -0700

    Initial commit

---
+++ Interface Styling /ContentView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            WelcomePage()
+            FeaturesPage()
+        }
+        .background(Gradient(colors: gradientColors))
+        .tabViewStyle(.page)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
